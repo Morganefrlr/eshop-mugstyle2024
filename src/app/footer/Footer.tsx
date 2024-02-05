bfeecbe4d88a3477fd5603a8f324575dedc3d477
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-02-05 11:15:13 +0100

    T03: footer html

--- src/app/footer/Footer.tsx
+++ src/app/footer/Footer.tsx
@@ -1,9 +1,30 @@
+import Link from "next/link";
+import Logo from "../reusableComponents/logo";
+import { footerMenuConfig } from "./footerMenuConfig";
 
 
 const Footer = () => {
     return (
-        <div>
-            je suis le footer
+        <div className="mainFooter">
+            <Logo />
+
+            <div>
+                {footerMenuConfig.map(item =>
+                <div key={item.label}>
+                    <h3>{item.label}</h3>
+                    {item.cat?.map(link =>
+                        <Link href={link.href} key={link.labelCat}>{link.labelCat}</Link>
+                    )}
+                    {item.desc && 
+                        <>
+                            <span>{item.desc}</span>
+                            <span>{item.mail}</span>
+                        </>
+                    }
+                </div>
+                
+                )}
+            </div>
         </div>
     );
 };
---
+++ src/app/reusableComponents/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <h1>MugStyle.</h1>
+    );
+};
+
+export default Logo;
