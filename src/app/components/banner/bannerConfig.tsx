e6ed7c8079257e14e28fd9094cf05e996e4b1205
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-02-05 13:49:14 +0100

    T04: banner styled

---
+++ src/app/components/banner/bannerConfig.tsx
@@ -0,0 +1,9 @@
+ 
+
+export const bannerConfig = {
+    title: "Mug Style",
+    subtitle:"best place to buy design",
+    desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus iaculis porta ex nec iaculis. Etiam ornare pellentesque elit vitae faucibus.',
+    buttonLabel:'explore our mug',
+    href:'/'
+}
