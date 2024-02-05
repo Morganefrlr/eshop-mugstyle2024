46c2209a883f2195c5578eb0b03a84fcc4434a3b
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-02-05 14:37:21 +0100

    T05: about styled

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -4,7 +4,7 @@ import { aboutConfig } from './aboutConfig';
 
 const About = () => {
     return (
-        <div>
+        <div className='aboutMain'>
             <h2>{aboutConfig.title}</h2>
             <p>{aboutConfig.desc}</p>
             <Link href={aboutConfig.href}>{aboutConfig.label}</Link>
