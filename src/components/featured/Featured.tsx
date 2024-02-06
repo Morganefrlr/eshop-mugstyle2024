fba4e39f201a318de7b77baf977b9d53d5560c50
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-02-06 15:25:04 +0100

    T06: featured title

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -5,11 +5,14 @@ import Card from '@/reusableComponents/card/Card';
 const Featured = () => {
     return (
         <div className='featuredMain'>
+            <p>Featured Mugs</p>
+            <div className='featuredCardsBox'>
             {featuredProducts.map(item =>
-            <div className='featuredMain_boxCard' key={item.title}>
+            <div className='featuredCardsBox_singleBox' key={item.title}>
                 <Card title={item.title} cover={item.cover} price={item.price} />
             </div>
             )}
+            </div>
         </div>
     );
 };
