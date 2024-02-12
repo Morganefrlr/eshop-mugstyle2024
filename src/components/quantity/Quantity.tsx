c66198517ff7cdc8db8e3ac32f19c73153ea5a3d
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-02-12 10:04:05 +0100

    T12: quantity styled

--- src/components/quantity/Quantity.tsx
+++ src/components/quantity/Quantity.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+'use client'
+import { useState } from 'react';
+import { MdOutlineKeyboardArrowDown, MdOutlineKeyboardArrowUp } from 'react-icons/md';
 
 const Quantity = () => {
+    const [quantity, setQuantity] = useState(1)
+
+
+
     return (
-        <div>
-            je suis la quantity
+        <div className="quantityMain">
+            <p>quantity</p>
+            <div className="quantityMain_container">
+                <div>
+                    <MdOutlineKeyboardArrowDown className="icon" onClick={() => setQuantity(prev =>(prev>1 ? prev-1 : 1))}/>
+                    <hr/>
+                    <p>{quantity}</p>
+                    <hr/>
+                    <MdOutlineKeyboardArrowUp  className="icon" onClick={() => setQuantity(prev =>(prev<9 ? prev+1 : 9))}/>
+                </div>
+                <button>add to cart</button>
+            </div>
         </div>
+
     );
 };
 
