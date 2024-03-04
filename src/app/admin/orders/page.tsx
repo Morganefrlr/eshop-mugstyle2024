44a36744d92f839f49049303ea0492e55b85acdb
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-03-04 11:17:52 +0100

    A05: order styled

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import { ordersList } from '@/fakeData/fakeData';
+import { HiMiniArrowsUpDown } from "react-icons/hi2";
 
 const page = () => {
+
+ 
     return (
-        <div>
-            je suis la page des orders
-        </div>
+        <div className="ordersMain">
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        Order ID
+                        <HiMiniArrowsUpDown className='icon' />
+                    </th>
+                    <th>
+                        Date
+                        <HiMiniArrowsUpDown className='icon'/>
+                    </th>
+                    <th>
+                        Price
+                        <HiMiniArrowsUpDown className='icon'/>
+                    </th>
+                    <th>
+                        Products
+                        <HiMiniArrowsUpDown className='icon'/>
+                    </th>
+                    <th>
+                        Status
+                        <HiMiniArrowsUpDown className='icon'/>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+               {ordersList.map(item =>
+                    <tr>
+                        <td>#{item.id}</td>
+                        <td>{item.createdAt}</td>
+                        <td>$ {item.price}</td>                          
+                        <td>{item.customer}</td>
+                        <td className={item.status}>
+                            <div>{item.status}</div></td>
+                        
+                    </tr>
+                )}
+                
+            </tbody>
+        </table>
+        
+    </div>
     );
 };
 
