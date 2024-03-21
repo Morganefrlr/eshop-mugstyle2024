344495719679c5d59a8eaafe93e41ee3e6a09b6d
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-03-21 11:09:28 +0100

    T16 : new product with slug & slugify

--- src/app/admin/products/adminPanel/AddForm.tsx
+++ src/app/admin/products/adminPanel/AddForm.tsx
@@ -1,23 +1,25 @@
 
 
+
+import { slugify } from '@/utils/slugify';
 import Form from './form/Form';
 import { useGlobalAdminContext } from '@/context/AdminContext';
 
 const AddForm = () => {
 
     const {productToEdit, setProductToEdit, handleAddProduct} = useGlobalAdminContext()
-
-
+   
     const handleChangeInputs = (e : any) => {
        const value = e.target.value
        setProductToEdit({
         ...productToEdit,
-        [e.target.name]: value
+        [e.target.name]: value,
        })
-      };
+    };
 
       const handleAddNewProduct = (e : any) => {
         e.preventDefault()
+        productToEdit.slug = slugify(productToEdit.title)
         handleAddProduct(productToEdit)
     }
     return (
