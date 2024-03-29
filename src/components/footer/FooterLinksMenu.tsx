a95ed1fb132ab4c56a3ff9498dd79f5b57427f3a
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-03-29 13:55:41 +0100

    REF : footer

--- src/components/footer/FooterLinksMenu.tsx
+++ src/components/footer/FooterLinksMenu.tsx
@@ -6,18 +6,18 @@ const FooterLinksMenu = () => {
     return (
         <div className="footerItems">
                 {footerMenuConfig.map(item =>
-                <div className="footerItems_column" key={item.label}>
-                    <h3>{item.label}</h3>
-                    {item.cat?.map(link =>
-                        <Link href={link.href} key={link.labelCat}>{link.labelCat}</Link>
-                    )}
-                    {item.desc && 
-                        <>
-                            <span>{item.desc}</span>
-                            <span>{item.mail}</span>
-                        </>
-                    }
-                </div>
+                    <div className="footerItems_column" key={item.label}>
+                        <h3>{item.label}</h3>
+                        {item.cat?.map(link =>
+                            <Link href={link.href} key={link.labelCat}>{link.labelCat}</Link>
+                        )}
+                        {item.desc && 
+                            <>
+                                <span>{item.desc}</span>
+                                <span>{item.mail}</span>
+                            </>
+                        }
+                    </div>
                 
                 )}
             </div>
