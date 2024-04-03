0575e5e2610c0f02b99881ea168cbc727b844fcf
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-04-03 11:38:59 +0200

    REF: cart

--- src/app/cart/cartContainer/CartContainer.tsx
+++ src/app/cart/cartContainer/CartContainer.tsx
@@ -1,24 +1,25 @@
-'use client'
-import { useGlobalAdminContext } from '@/context/AdminContext';
+
 import CartLeftSide from './cartLeftSide/CartLeftSide';
 import CartRightSide from './cartRightSide/CartRightSide';
+import { ProductType } from '@/fakeData/typeData';
 
-
-const CartContainer = () => {
-    const { cart, quantityProducts, totalPrice, handleDeleteProductToCart } = useGlobalAdminContext()
-
-    const handleRemoveProductCart = (slug : string) => {
-        handleDeleteProductToCart(slug)
-    }
+type Props = {
+    quantity: number,
+    totalPrice: number,
+    cart : ProductType[],
+    deleteBtn: (a:string) => void,
+}
+const CartContainer = ({quantity, totalPrice, cart, deleteBtn} : Props) => {
+    
 
     return (
         <div className='cartBottom'>
             <div className='cartBottom_leftSide'>
-                <CartLeftSide cart={cart} deleteBtn={handleRemoveProductCart}/>
+                <CartLeftSide cart={cart} deleteBtn={deleteBtn}/>
             </div>
             <hr />
             <div className='cartBottom_rightSide'>
-                <CartRightSide quantity={quantityProducts} totalPrice={totalPrice} />
+                <CartRightSide quantity={quantity} totalPrice={totalPrice} />
             </div>
         </div>
     );
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,15 +1,20 @@
-
+'use client'
 import Title from '@/reusableComponents/title/Title';
 import CartContainer from './cartContainer/CartContainer';
+import { useGlobalAdminContext } from '@/context/AdminContext';
 
 
 
 const page = () => {
+    const { cart, quantityProducts, totalPrice, handleDeleteProductToCart } = useGlobalAdminContext()
 
+    const handleRemoveProductCart = (slug : string) => {
+        handleDeleteProductToCart(slug)
+    }
     return (
         <div className='cartMain'>
             <Title label={'Your Cart'} titlePage={true}/>
-            <CartContainer />
+            <CartContainer cart={cart} deleteBtn={handleRemoveProductCart} quantity={quantityProducts} totalPrice={totalPrice} />
         </div>
     );
 };
