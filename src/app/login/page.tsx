373971da5a4e36b36b3e3823100dc4fa4241e875
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-05-31 14:42:09 +0200

    B01: register front component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,8 +7,23 @@ const page = () => {
 
     return (
         <div className="loginMain">
-            <Image alt="login" src='/m1-b.jpg' width={500} height={500} />
-            <LoginComponent />
+            <h1>Register</h1>
+
+            <div className="loginInputContainer">
+                <label>Username</label>
+                <input type="text" name="username" placeholder="Username" />
+            </div>
+            <div className="loginInputContainer">
+                <label>E-mail</label>
+                <input type="mail" name="mail" placeholder="E-mail" />
+            </div>
+            <div className="loginInputContainer">
+                <label>Password</label>
+                <input type="password" name="Password" placeholder="Password" />
+            </div>
+
+            <button>Register</button>
+            <p>Have an account?</p>
         </div>
     );
 };
