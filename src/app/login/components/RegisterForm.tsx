45154b8610a879be02ffe9b38d062ebaae1c4b18
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-06-03 11:02:05 +0200

    B01: register refecto

---
+++ src/app/login/components/RegisterForm.tsx
@@ -0,0 +1,28 @@
+
+
+const RegisterForm = () => {
+    return (
+        <div className="containerRegister">
+                <h1>Register</h1>
+                <div className="containerInputs">
+                    <div className="loginInputContainer">
+                        <label>Username</label>
+                        <input type="text" name="username" placeholder="Username" />
+                    </div>
+                    <div className="loginInputContainer">
+                        <label>E-mail</label>
+                        <input type="mail" name="mail" placeholder="E-mail" />
+                    </div>
+                    <div className="loginInputContainer">
+                        <label>Password</label>
+                        <input type="password" name="Password" placeholder="Password" />
+                    </div>
+                </div>
+
+                <button>Register</button>
+                <p>Have an account?</p>
+        </div>
+    );
+};
+
+export default RegisterForm;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,32 +1,14 @@
 
 import Image from "next/image";
 import LoginComponent from "@/app/login/loginComponent/LoginComponent";
+import RegisterForm from "./components/RegisterForm";
 
 
 const page = () => {
 
     return (
         <div className="loginMain">
-            <div className="containerRegister">
-                <h1>Register</h1>
-                <div className="containerInputs">
-                    <div className="loginInputContainer">
-                        <label>Username</label>
-                        <input type="text" name="username" placeholder="Username" />
-                    </div>
-                    <div className="loginInputContainer">
-                        <label>E-mail</label>
-                        <input type="mail" name="mail" placeholder="E-mail" />
-                    </div>
-                    <div className="loginInputContainer">
-                        <label>Password</label>
-                        <input type="password" name="Password" placeholder="Password" />
-                    </div>
-                </div>
-
-                <button>Register</button>
-                <p>Have an account?</p>
-            </div>
+            <RegisterForm />
         </div>
     );
 };
