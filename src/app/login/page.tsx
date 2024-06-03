8539e4cb592f66c4d0be9c5a99bd1b1dadd6ef4d
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2024-06-03 10:59:48 +0200

    B01: styled register

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,23 +7,26 @@ const page = () => {
 
     return (
         <div className="loginMain">
-            <h1>Register</h1>
+            <div className="containerRegister">
+                <h1>Register</h1>
+                <div className="containerInputs">
+                    <div className="loginInputContainer">
+                        <label>Username</label>
+                        <input type="text" name="username" placeholder="Username" />
+                    </div>
+                    <div className="loginInputContainer">
+                        <label>E-mail</label>
+                        <input type="mail" name="mail" placeholder="E-mail" />
+                    </div>
+                    <div className="loginInputContainer">
+                        <label>Password</label>
+                        <input type="password" name="Password" placeholder="Password" />
+                    </div>
+                </div>
 
-            <div className="loginInputContainer">
-                <label>Username</label>
-                <input type="text" name="username" placeholder="Username" />
+                <button>Register</button>
+                <p>Have an account?</p>
             </div>
-            <div className="loginInputContainer">
-                <label>E-mail</label>
-                <input type="mail" name="mail" placeholder="E-mail" />
-            </div>
-            <div className="loginInputContainer">
-                <label>Password</label>
-                <input type="password" name="Password" placeholder="Password" />
-            </div>
-
-            <button>Register</button>
-            <p>Have an account?</p>
         </div>
     );
 };
